8a375a75377ce10c79c8ed9b73678a9a03f85db3
John Taylor <john.taylor@doverfs.com>
2021-02-26 18:04:26 -0700

    Initial Push

---
+++ SBListener/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.Threading;
+
+namespace SBListener
+{
+    public class Program
+    {
+        private static CancellationTokenSource _cancelKeyPressed = new CancellationTokenSource();
+
+        public static int Main(string[] args)
+        {
+            Console.CancelKeyPress += Console_CancelKeyPress;
+
+            var cliApp = new CommandLineApplication(false);
+
+            cliApp.HelpOption("-h|-?|--help");
+
+            var endPointParam = cliApp.Option("-e | --end-point <end-point>",
+                "The end point of the service bus", CommandOptionType.SingleValue);
+
+            cliApp.Command("topic", cmd =>
+            {
+                cmd.Description = "Listen to a service bus topic subscription";
+                cmd.HelpOption("-h|-?|--help");
+                var topicArg = cmd.Argument("[topic]", "Service bus topic to listen to");
+                var subscriptionArg = cmd.Argument("[subscription]", "Service bus topic subscription to listen to");
+
+                cmd.OnExecute(async () =>
+                {
+                    if (!endPointParam.HasValue() || string.IsNullOrEmpty(topicArg.Value) || string.IsNullOrEmpty(subscriptionArg.Value))
+                    {
+                        cmd.ShowHelp();
+                        return 0;
+                    }
+
+                    var connectionString = ConnectionString(endPointParam);
+                    await new SBTopicListener(connectionString, topicArg.Value, subscriptionArg.Value).Listen(_cancelKeyPressed.Token);
+
+                    return 0;
+                });
+            });
+
+            cliApp.OnExecute(() =>
+            {
+                cliApp.ShowHelp();
+                return 0;
+            });
+
+            return cliApp.Execute(args);
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            _cancelKeyPressed.Cancel();
+        }
+
+        private static string ConnectionString(
+            CommandOption endPointParam)
+        {
+            if (endPointParam.HasValue())
+            {
+                return endPointParam.Value().Trim('\'');
+            }
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ SBListener/SBTopicListener.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SBListener
+{
+    public class SBTopicListener
+    {
+        private readonly string _connectionString;
+        private readonly string _topic;
+        private readonly string _subscription;
+        private readonly StreamWriter _stderr;
+
+        public SBTopicListener(string connectionString, string topic, string subscription)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new System.ArgumentException($"'{nameof(connectionString)}' cannot be null or whitespace.", nameof(connectionString));
+            }
+
+            if (string.IsNullOrWhiteSpace(topic))
+            {
+                throw new System.ArgumentException($"'{nameof(topic)}' cannot be null or whitespace.", nameof(topic));
+            }
+
+            _connectionString = connectionString;
+            _topic = topic;
+            _subscription = subscription;
+
+            _stderr = new StreamWriter(Console.OpenStandardError());
+        }
+
+        public async Task Listen(CancellationToken cancellationToken)
+        {
+            var client = new SubscriptionClient(
+                _connectionString,
+                _topic,
+                _subscription,
+                ReceiveMode.ReceiveAndDelete,
+                new RetryExponential(TimeSpan.FromMilliseconds(250), TimeSpan.FromMinutes(5), 3));
+
+            client.RegisterMessageHandler(ClientHandler, ErrorHandler);
+
+            await Task.Delay(Timeout.Infinite, cancellationToken);
+
+            await client.UnregisterMessageHandlerAsync(TimeSpan.FromMinutes(5));
+        }
+
+        private static Task ClientHandler(Message arg1, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"{Encoding.UTF8.GetString(arg1.Body)}");
+            return Task.CompletedTask;
+        }
+
+        private Task ErrorHandler(ExceptionReceivedEventArgs arg)
+        {
+            _stderr.WriteLine($"Error: {JsonConvert.SerializeObject(arg)}");
+            return Task.CompletedTask;
+        }
+
+    }
+}
